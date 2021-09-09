40ee64f1f782bd4ecb043460be93adc05bdb234f
Art Zwinger <artzwinger@gmail.com>
2021-09-09 19:04:36 +0300

    Landing Page: Text Update

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
   <div class="container vh-100 d-flex align-items-center justify-content-center">
     <div class="jumbotron text-center" style="background: #f2efed">
       <h1 class="display-4" style="color: #f75845">THANK YOU!</h1>
-      <p class="lead">We work on your request.</p>
+      <p class="lead">We are working on your request.</p>
       <hr>
       <p>Having trouble? <a href="mailto:sales@onlineclasshelp.com">Contact Us</a></p>
       <p class="lead">
