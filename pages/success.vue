ee7696708a0e59413bb1dde06b5e8a31a08484fe
Art Zwinger <artzwinger@gmail.com>
2021-09-29 15:53:59 +0300

    Landing Page: Text Update

--- pages/success.vue
+++ pages/success.vue
@@ -2,7 +2,7 @@
   <div class="container vh-100 d-flex align-items-center justify-content-center">
     <div class="jumbotron text-center" style="background: #f2efed">
       <h1 class="display-4" style="color: #f75845">THANK YOU!</h1>
-      <p class="lead">We have received your submission.</p>
+      <p class="lead">We are working on your request.</p>
       <hr>
       <p>Having trouble? <nuxt-link to="contact">Contact Us</nuxt-link></p>
       <p class="lead">
